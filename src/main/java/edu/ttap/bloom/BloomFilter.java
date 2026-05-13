5efde82c692ea344e4c7b4b1fa0a440c01a6d740
leria-kosse <leria.kosse@gmail.com>
2026-05-13 11:15:18 -0500

    complete

--- src/main/java/edu/ttap/bloom/BloomFilter.java
+++ src/main/java/edu/ttap/bloom/BloomFilter.java
@@ -3,7 +3,7 @@ package edu.ttap.bloom;
 import java.util.BitSet;
 import java.util.List;
 import java.util.function.Function;
-import java.util.BitSet;
+
 /**
  * A Bloom Filter is a probabilistic data structure that efficiently tests
  * set membership with the possibility of false positives.
@@ -11,9 +11,15 @@ import java.util.BitSet;
 public class BloomFilter<T> {
 
     private BitSet bits;
+
     private int size;
+
     private List<Function<T, Integer>> hashFunctions;
 
+    /**
+     * @param size the number of bits in the filter
+     * @param hashFunctions the list of hash functions to use
+     */
     public BloomFilter(int size, List<Function<T, Integer>> hashFunctions) {
         this.size = size;
         this.bits = new BitSet(size);
@@ -26,23 +32,22 @@ public class BloomFilter<T> {
             int hash = hashFunction.apply(item);
             int index = Math.floorMod(hash, size);
             bits.set(index);
+        }
     }
-}
 
     /**
      * @param item the item to check for membership in the Bloom filter
      * @return true if the item is (possibly) in the Bloom filter and false if
-     * it is definitely not in the filter.
+     *     it is definitely not in the filter.
      */
     public boolean contains(T item) {
         for (Function<T, Integer> hashFunction : hashFunctions) {
             int hash = hashFunction.apply(item);
             int index = Math.floorMod(hash, size);
-
             if (!bits.get(index)) {
                 return false;
             }
         }
-    return true;
+        return true;
     }
 }
--- src/main/java/edu/ttap/bloom/MaliciousURLDetector.java
+++ src/main/java/edu/ttap/bloom/MaliciousURLDetector.java
@@ -1,6 +1,5 @@
 package edu.ttap.bloom;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
@@ -10,7 +9,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
-
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
@@ -20,6 +18,7 @@ import com.google.common.hash.Hashing;
  * potentially malicious.
  */
 public class MaliciousURLDetector {
+
     // From: https://www.kaggle.com/datasets/sid321axn/malicious-urls-dataset
     public static final String DATA_PATH = "data/malicious_phish.csv";
 
@@ -31,84 +30,74 @@ public class MaliciousURLDetector {
      */
     public static List<Function<String, Integer>> makeStringHashFunctions(int num) {
         List<Function<String, Integer>> functions = new ArrayList<>();
-
         for (int i = 0; i < num; i++) {
             int seed = ThreadLocalRandom.current().nextInt();
             HashFunction hashFunction = Hashing.murmur3_128(seed);
-
             Function<String, Integer> f = input ->
-                hashFunction.hashString(input, Charset.defaultCharset()).asInt();
-
+                    hashFunction.hashString(input, Charset.defaultCharset()).asInt();
             functions.add(f);
         }
-
         return functions;
     }
 
     /**
-     * @param numBits the number of bits dedicated to the filter
+     * @param filename the path to the CSV file
+     * @param bitSetSize the number of bits dedicated to the filter
      * @param numHashFunctions the number of hash functions to use
      * @return a Bloom filter for detecting malicious URLs.
+     * @throws IOException if the file cannot be read
      */
-    public static BloomFilter<String> makeURLFilter(String filename, int bitSetSize, int numHashFunctions) 
-    throws IOException {
+    public static BloomFilter<String> makeURLFilter(
+            String filename, int bitSetSize, int numHashFunctions)
+            throws IOException {
         List<Function<String, Integer>> hashFunctions = makeStringHashFunctions(numHashFunctions);
         BloomFilter<String> filter = new BloomFilter<>(bitSetSize, hashFunctions);
         List<String> lines = Files.readAllLines(Path.of(filename));
-
         for (String line : lines) {
-            if (line.equals("url,type")) {continue;}
+            if (line.equals("url,type")) {
+                continue;
+            }
             String[] parts = line.split(",");
-
             if (parts.length >= 2) {
                 String url = parts[0];
                 String type = parts[1];
-
-                if (!type.equals("benign")) {filter.add(url);}
+                if (!type.equals("benign")) {
+                    filter.add(url);
+                }
             }
         }
-
         return filter;
     }
 
     /**
      * The main method for the program.
      * @param args the arguments to the program
-     * @throws FileNotFoundException
+     * @throws IOException if the data file cannot be read
      */
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
-            System.out.println("Usage: mvn compile exec:java -Dexec.args=\"<bitset size> <num hash functions>\"");
+            System.out.println(
+                "Usage: mvn compile exec:java -Dexec.args=\"<bitset size> <# hash functions>\"");
             return;
         }
-
         int bitSetSize = Integer.parseInt(args[0]);
         int numHashFunctions = Integer.parseInt(args[1]);
-
         BloomFilter<String> filter =
-            makeURLFilter("data/malicious_phish.csv", bitSetSize, numHashFunctions);
-
+                makeURLFilter("data/malicious_phish.csv", bitSetSize, numHashFunctions);
         Scanner scanner = new Scanner(System.in);
-
         while (true) {
             System.out.println("Enter a URL to check (or \"exit\" to quit):");
             System.out.print("> ");
-
             String input = scanner.nextLine();
-
             if (input.equals("exit")) {
                 break;
             }
-
             if (filter.contains(input)) {
-                System.out.println("‼️ The URL is possibly malicious..."); 
-                //holy chatgpt emojis
+                System.out.println("The URL is possibly malicious!!!");
             } else {
-                System.out.println("✅ The URL is not known to be malicious..."); 
-                //holy chatgpt checkmark
+                System.out.println("The URL is not known to be malicious...");
             }
         }
-
         scanner.close();
     }
 }
